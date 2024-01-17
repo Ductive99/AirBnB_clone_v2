e17c236051daecc767b9774ff927b15e5348fbd8
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-17 14:55:30 +0100

    Added parameter object creation

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,18 +113,39 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+    def do_create(self, line):
+        """ Create an object of any class """
+        try:
+            if not line:
+                raise SyntaxError()
+
+            args = line.split(" ")
+            if args:
+                cls_name = args[0]
+            else:
+                raise SyntaxError()
+
+            obj = HBNBCommand.classes[cls_name]()
+
+            for param in args[1:]:
+                if '=' in param:
+                    k, v = param.split("=", 1)
+                    if self.is_int(v):
+                        v = int(v)
+                    elif self.is_float(v):
+                        v = float(v)
+                    elif v[0] == '"' and v[-1] == '"':
+                        v = v[1:-1].replace('_', ' ').replace('\\"', '"')
+                    setattr(obj, k, v)
+
+            storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        except KeyError:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -187,7 +208,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +293,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +301,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] != '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +340,25 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    @staticmethod
+    def is_int(v):
+        """ Checks if a given value is an integer """
+        try:
+            int(v)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def is_float(v):
+        """ Checks if a given value is a float """
+        try:
+            float(v)
+            return True
+        except ValueError:
+            return False
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

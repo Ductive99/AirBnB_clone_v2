d3ecb3e28da460a3a6f5f6611a22ec9d562bc534
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-17 10:49:20 +0100

    Added first web page using Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,3 +3,11 @@
 Script that starts a Flask web application
 web app is listening on 0.0.0.0, port 5000
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
